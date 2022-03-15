255157a17876f848d4c917b22f134ee7c576c1b5
zhouzhishou <zhouzhishou_123@163.com>
2022-03-15 15:09:19 +0800

    归并排序

--- src/Algorithms/Sort/MergeSort.ts
+++ src/Algorithms/Sort/MergeSort.ts
@@ -8,19 +8,42 @@ import Sort from './Sort'
 
 class MergeSort extends Sort {
     protected sort(): void {
-
+        this.mergeSort(0, this.array.length)
     }
     /**
      * @param {number} begin
      * @param {number} end
      * @return {*}
      * @Description: 对[begin, end]
-     */    
-    mergeSort(begin: number, end: number):void {
+     */
+    mergeSort(begin: number, end: number): void {
+        // 分割成一个元素
+        if (end - begin < 2) return
+        let mid = (begin + end) >> 1
+        // 对左一半进行分割直到分割成一个元素
+        this.mergeSort(begin, mid)
+        // 对右一半进行分割直到分割成一个元素
+        this.mergeSort(mid, end)
 
+        // 对分割的元素排序合并
+        this.merge(begin, mid, end)
     }
-    merge(begin:number, mid:number, end: number){
-        
+    merge(begin: number, mid: number, end: number) {
+        let li = 0, le = mid - begin
+        let ri = mid, re = end
+        let ai = begin
+        // 备份左半部分的数据
+        let leftArray = []
+        for (let i = li; i < le; i++) {
+            leftArray[i] = this.array[begin + i]
+        }
+        while (li < le) {
+            if (ri < re && this.array[ri] - leftArray[li] < 0) {
+                this.array[ai++] = this.array[ri++]
+            } else {
+                this.array[ai++] = leftArray[li++]
+            }
+        }
     }
 }
 
